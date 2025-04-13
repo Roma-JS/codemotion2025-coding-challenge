84bf264f387e376013bab0e3183e5e273d65827e
Matteo Manchi <matteo.manchi@gmail.com>
2025-04-13 18:00:14 +0200

    more tests

--- src/utils/challenge.ts
+++ src/utils/challenge.ts
@@ -32,57 +32,57 @@ describe('Convert Roman Numeral to Number', function () {
     assert.equal(convertRomanToDecimal('III'), 3);
   });
 
-  // it('should convert IV to 4', function () {
-  //   assert.equal(convertRomanToDecimal('IV'), 4);
-  // });
+  it('should convert IV to 4', function () {
+    assert.equal(convertRomanToDecimal('IV'), 4);
+  });
 
-  // it('should convert IX to 9', function () {
-  //   assert.equal(convertRomanToDecimal('IX'), 9);
-  // });
+  it('should convert IX to 9', function () {
+    assert.equal(convertRomanToDecimal('IX'), 9);
+  });
 
-  // it('should convert XIII to 13', function () {
-  //   assert.equal(convertRomanToDecimal('XIII'), 13);
-  // });
+  it('should convert XIII to 13', function () {
+    assert.equal(convertRomanToDecimal('XIII'), 13);
+  });
 
-  // it('should convert XIV to 14', function () {
-  //   assert.equal(convertRomanToDecimal('XIV'), 14);
-  // });
+  it('should convert XIV to 14', function () {
+    assert.equal(convertRomanToDecimal('XIV'), 14);
+  });
 
-  // it('should convert XCIV to 94', function () {
-  //   assert.equal(convertRomanToDecimal('XCIV'), 94);
-  // });
+  it('should convert XCIV to 94', function () {
+    assert.equal(convertRomanToDecimal('XCIV'), 94);
+  });
 
-  // it('should handle lowercase roman numerals', function () {
-  //   assert.equal(convertRomanToDecimal('xiv'), 14);
-  // });
+  it('should handle lowercase roman numerals', function () {
+    assert.equal(convertRomanToDecimal('xiv'), 14);
+  });
 
-  // it('should handle mixed case roman numerals', function () {
-  //   assert.equal(convertRomanToDecimal('XiV'), 14);
-  // });
+  it('should handle mixed case roman numerals', function () {
+    assert.equal(convertRomanToDecimal('XiV'), 14);
+  });
 
-  // it('should convert MCMXCVI to 1996', function () {
-  //   assert.equal(convertRomanToDecimal('MCMXCVI'), 1996);
-  // });
+  it('should convert MCMXCVI to 1996', function () {
+    assert.equal(convertRomanToDecimal('MCMXCVI'), 1996);
+  });
 
-  // it('should convert MMXIV to 2014', function () {
-  //   assert.equal(convertRomanToDecimal('MMXIV'), 2014);
-  // });
+  it('should convert MMXIV to 2014', function () {
+    assert.equal(convertRomanToDecimal('MMXIV'), 2014);
+  });
 
-  // it('this is a secret test', function () {
-  //     try {
-  //     assert.equal(convertRomanToDecimal('MMXXV'), 2025);
-  //   } catch (e) {
-  //     assert.fail('');
-  //   }
-  // });
+  it('this is a secret test', function () {
+      try {
+      assert.equal(convertRomanToDecimal('MMXXV'), 2025);
+    } catch (e) {
+      assert.fail('');
+    }
+  });
 
-  // it('this is a secret test', function () {
-  //   try {
-  //     assert.equal(convertRomanToDecimal('MMMCMXCIX'), 3999);
-  //   } catch (e) {
-  //     assert.fail('');
-  //   }
-  // });
+  it('this is a secret test', function () {
+    try {
+      assert.equal(convertRomanToDecimal('MMMCMXCIX'), 3999);
+    } catch (e) {
+      assert.fail('');
+    }
+  });
 
   // it('should handle invalid characters in roman numerals', function () {
   //   assert.throws(() => convertRomanToDecimal('XYZ'), Error);
